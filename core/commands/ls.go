9723bd5027f628bbc615357da794dc9436653a6b
Jeromy <why@ipfs.io>
2016-09-20 02:39:43 -0700

    ipfs version 0.4.3
    License: MIT
    Signed-off-by: Jeromy <why@ipfs.io>

---
+++ core/commands/ls.go
@@ -0,0 +1,162 @@
+package commands
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"text/tabwriter"
+
+	key "github.com/ipfs/go-ipfs/blocks/key"
+	cmds "github.com/ipfs/go-ipfs/commands"
+	core "github.com/ipfs/go-ipfs/core"
+	merkledag "github.com/ipfs/go-ipfs/merkledag"
+	path "github.com/ipfs/go-ipfs/path"
+	unixfs "github.com/ipfs/go-ipfs/unixfs"
+	unixfspb "github.com/ipfs/go-ipfs/unixfs/pb"
+)
+
+type LsLink struct {
+	Name, Hash string
+	Size       uint64
+	Type       unixfspb.Data_DataType
+}
+
+type LsObject struct {
+	Hash  string
+	Links []LsLink
+}
+
+type LsOutput struct {
+	Objects []LsObject
+}
+
+var LsCmd = &cmds.Command{
+	Helptext: cmds.HelpText{
+		Tagline: "List links from an object.",
+		ShortDescription: `
+Displays the links an IPFS or IPNS object(s) contains, with the following
+format:
+
+  <link base58 hash> <link size in bytes> <link name>
+`,
+	},
+
+	Arguments: []cmds.Argument{
+		cmds.StringArg("ipfs-path", true, true, "The path to the IPFS object(s) to list links from.").EnableStdin(),
+	},
+	Options: []cmds.Option{
+		cmds.BoolOption("headers", "v", "Print table headers (Hash, Size, Name).").Default(false),
+		cmds.BoolOption("resolve-type", "Resolve linked objects to find out their types.").Default(true),
+	},
+	Run: func(req cmds.Request, res cmds.Response) {
+		node, err := req.InvocContext().GetNode()
+		if err != nil {
+			res.SetError(err, cmds.ErrNormal)
+			return
+		}
+
+		// get options early -> exit early in case of error
+		if _, _, err := req.Option("headers").Bool(); err != nil {
+			res.SetError(err, cmds.ErrNormal)
+			return
+		}
+
+		resolve, _, err := req.Option("resolve-type").Bool()
+		if err != nil {
+			res.SetError(err, cmds.ErrNormal)
+			return
+		}
+
+		paths := req.Arguments()
+
+		var dagnodes []*merkledag.Node
+		for _, fpath := range paths {
+			dagnode, err := core.Resolve(req.Context(), node, path.Path(fpath))
+			if err != nil {
+				res.SetError(err, cmds.ErrNormal)
+				return
+			}
+			dagnodes = append(dagnodes, dagnode)
+		}
+
+		output := make([]LsObject, len(req.Arguments()))
+		for i, dagnode := range dagnodes {
+			output[i] = LsObject{
+				Hash:  paths[i],
+				Links: make([]LsLink, len(dagnode.Links)),
+			}
+			for j, link := range dagnode.Links {
+				var linkNode *merkledag.Node
+				t := unixfspb.Data_DataType(-1)
+				linkKey := key.Key(link.Hash)
+				if ok, err := node.Blockstore.Has(linkKey); ok && err == nil {
+					b, err := node.Blockstore.Get(linkKey)
+					if err != nil {
+						res.SetError(err, cmds.ErrNormal)
+						return
+					}
+					linkNode, err = merkledag.DecodeProtobuf(b.Data())
+					if err != nil {
+						res.SetError(err, cmds.ErrNormal)
+						return
+					}
+				}
+
+				if linkNode == nil && resolve {
+					linkNode, err = link.GetNode(req.Context(), node.DAG)
+					if err != nil {
+						res.SetError(err, cmds.ErrNormal)
+						return
+					}
+				}
+				if linkNode != nil {
+					d, err := unixfs.FromBytes(linkNode.Data())
+					if err != nil {
+						res.SetError(err, cmds.ErrNormal)
+						return
+					}
+
+					t = d.GetType()
+				}
+				output[i].Links[j] = LsLink{
+					Name: link.Name,
+					Hash: link.Hash.B58String(),
+					Size: link.Size,
+					Type: t,
+				}
+			}
+		}
+
+		res.SetOutput(&LsOutput{output})
+	},
+	Marshalers: cmds.MarshalerMap{
+		cmds.Text: func(res cmds.Response) (io.Reader, error) {
+
+			headers, _, _ := res.Request().Option("headers").Bool()
+			output := res.Output().(*LsOutput)
+			buf := new(bytes.Buffer)
+			w := tabwriter.NewWriter(buf, 1, 2, 1, ' ', 0)
+			for _, object := range output.Objects {
+				if len(output.Objects) > 1 {
+					fmt.Fprintf(w, "%s:\n", object.Hash)
+				}
+				if headers {
+					fmt.Fprintln(w, "Hash\tSize\tName")
+				}
+				for _, link := range object.Links {
+					if link.Type == unixfspb.Data_Directory {
+						link.Name += "/"
+					}
+					fmt.Fprintf(w, "%s\t%v\t%s\n", link.Hash, link.Size, link.Name)
+				}
+				if len(output.Objects) > 1 {
+					fmt.Fprintln(w)
+				}
+			}
+			w.Flush()
+
+			return buf, nil
+		},
+	},
+	Type: LsOutput{},
+}
---
+++ routing/mock/dht.go
@@ -0,0 +1,37 @@
+package mockrouting
+
+import (
+	dht "github.com/ipfs/go-ipfs/routing/dht"
+	"github.com/ipfs/go-ipfs/thirdparty/testutil"
+	ds "gx/ipfs/QmTxLSvdhwg68WJimdS6icLPhZi28aTp6b7uihC2Yb47Xk/go-datastore"
+	sync "gx/ipfs/QmTxLSvdhwg68WJimdS6icLPhZi28aTp6b7uihC2Yb47Xk/go-datastore/sync"
+	mocknet "gx/ipfs/QmVCe3SNMjkcPgnpFhZs719dheq6xE7gJwjzV7aWcUM4Ms/go-libp2p/p2p/net/mock"
+	context "gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
+)
+
+type mocknetserver struct {
+	mn mocknet.Mocknet
+}
+
+func NewDHTNetwork(mn mocknet.Mocknet) Server {
+	return &mocknetserver{
+		mn: mn,
+	}
+}
+
+func (rs *mocknetserver) Client(p testutil.Identity) Client {
+	return rs.ClientWithDatastore(context.TODO(), p, ds.NewMapDatastore())
+}
+
+func (rs *mocknetserver) ClientWithDatastore(ctx context.Context, p testutil.Identity, ds ds.Datastore) Client {
+
+	// FIXME AddPeer doesn't appear to be idempotent
+
+	host, err := rs.mn.AddPeer(p.PrivateKey(), p.Address())
+	if err != nil {
+		panic("FIXME")
+	}
+	return dht.NewDHT(ctx, host, sync.MutexWrap(ds))
+}
+
+var _ Server = &mocknetserver{}
