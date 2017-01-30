5d93f5272168156362e75d75c23667698a1e6f48
Jeromy <jeromyj@gmail.com>
2017-01-29 23:20:28 -0800

    Ipfs 0.4.5, release candidate 1
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

---
+++ core/mock/mock.go
@@ -0,0 +1,77 @@
+package coremock
+
+import (
+	"context"
+	"net"
+
+	commands "github.com/ipfs/go-ipfs/commands"
+	core "github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/repo"
+	config "github.com/ipfs/go-ipfs/repo/config"
+	ds2 "github.com/ipfs/go-ipfs/thirdparty/datastore2"
+	testutil "github.com/ipfs/go-ipfs/thirdparty/testutil"
+
+	host "gx/ipfs/QmPsRtodRuBUir32nz5v4zuSBTSszrR1d3fA6Ahb6eaejj/go-libp2p-host"
+	"gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
+	syncds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/sync"
+	metrics "gx/ipfs/QmY2otvyPM2sTaDsczo7Yuosg98sUMCJ9qx1gpPaAPTS9B/go-libp2p-metrics"
+	mocknet "gx/ipfs/QmdzDdLZ7nj133QvNHypyS9Y39g35bMFk5DJ2pmX7YqtKU/go-libp2p/p2p/net/mock"
+	pstore "gx/ipfs/QmeXj9VAjmYQZxpmVz7VzccbJrpmr8qkCDSjfVNsPTWTYU/go-libp2p-peerstore"
+	smux "gx/ipfs/QmeZBgYBHvxMukGK5ojg28BCNLB9SeXqT7XXg6o7r2GbJy/go-stream-muxer"
+	peer "gx/ipfs/QmfMmLGoKzCHDN7cGgk64PJr4iipzidDRME8HABSJqvmhC/go-libp2p-peer"
+)
+
+// NewMockNode constructs an IpfsNode for use in tests.
+func NewMockNode() (*core.IpfsNode, error) {
+	ctx := context.Background()
+
+	// effectively offline, only peer in its network
+	return core.NewNode(ctx, &core.BuildCfg{
+		Online: true,
+		Host:   MockHostOption(mocknet.New(ctx)),
+	})
+}
+
+func MockHostOption(mn mocknet.Mocknet) core.HostOption {
+	return func(ctx context.Context, id peer.ID, ps pstore.Peerstore, bwr metrics.Reporter, fs []*net.IPNet, _ smux.Transport) (host.Host, error) {
+		return mn.AddPeerWithPeerstore(id, ps)
+	}
+}
+
+func MockCmdsCtx() (commands.Context, error) {
+	// Generate Identity
+	ident, err := testutil.RandIdentity()
+	if err != nil {
+		return commands.Context{}, err
+	}
+	p := ident.ID()
+
+	conf := config.Config{
+		Identity: config.Identity{
+			PeerID: p.String(),
+		},
+	}
+
+	r := &repo.Mock{
+		D: ds2.CloserWrap(syncds.MutexWrap(datastore.NewMapDatastore())),
+		C: conf,
+	}
+
+	node, err := core.NewNode(context.Background(), &core.BuildCfg{
+		Repo: r,
+	})
+	if err != nil {
+		return commands.Context{}, err
+	}
+
+	return commands.Context{
+		Online:     true,
+		ConfigRoot: "/tmp/.mockipfsconfig",
+		LoadConfig: func(path string) (*config.Config, error) {
+			return &conf, nil
+		},
+		ConstructNode: func() (*core.IpfsNode, error) {
+			return node, nil
+		},
+	}, nil
+}
